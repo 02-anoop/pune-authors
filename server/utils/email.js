7c5721dee3783dba31828a4ce354a4b22017275f
Anoop Saini <anoopsaini.0905@gmail.com>
2026-08-03 19:58:51 +0530

    Update Titan SMTP to use port 587

--- server/utils/email.js
+++ server/utils/email.js
@@ -12,8 +12,8 @@ if (process.env.EMAIL_USER && process.env.EMAIL_PASS) {
     // Titan Mail (Hostinger/GoDaddy) SMTP settings
     mailTransporter = nodemailer.createTransport({
       host: 'smtp.titan.email',
-      port: 465,
-      secure: true,
+      port: 587,
+      secure: false,
       auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS }
     });
   }
